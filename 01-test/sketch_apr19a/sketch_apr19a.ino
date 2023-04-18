508b1ea209e8a5d74f193995427e3939b12eb0bb
winetree94 <winetree94@gmail.com>
2023-04-19 01:56:45 +0900

    update

--- 01-test/sketch_apr19a/sketch_apr19a.ino
+++ 01-test/sketch_apr19a/sketch_apr19a.ino
@@ -1,37 +1,66 @@
-/*
-  Blink
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
-  Turns an LED on for one second, then off for one second, repeatedly.
+#define Rows 4
+#define Cols 4
 
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
+char keys[Rows][Cols] = {
+  { '1', '2', '3', 'A' },
+  { '4', '5', '6', 'B' },
+  { '7', '8', '9', 'C' },
+  { '*', '0', '#', 'D' }
+};
 
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
+#define MAX_CHAR 16
+#define MAX_LINE 2
 
-  This example code is in the public domain.
+LiquidCrystal_I2C lcd(0x27, MAX_CHAR, MAX_LINE);
 
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
-*/
+const int RowPinTbl[Rows] = { 5, 4, 3, 2 };
+const int ColPinTbl[Cols] = { 6, 7, 8, 9 };
 
-// the setup function runs once when you press reset or power the board
+// 직전 키
+char previousKey;
+bool pressed;
+
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  // 디스플레이 초가화
+  lcd.begin();
+  // 디스플레이 백라이트 활성화
+  lcd.backlight();
+  // input 초기화
+  lcd.setCursor(0, 0);
+  lcd.print("13 + 15 = 28");
+
+  Serial.begin(9600);
+  for (int i = 0; i < Rows; i++) {
+    pinMode(RowPinTbl[i], INPUT_PULLUP);
+  }
+
+  for (int j = 0; j < Cols; j++) {
+    pinMode(ColPinTbl[j], OUTPUT);
+    digitalWrite(ColPinTbl[j], HIGH);
+  }
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(1000);                      // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-  delay(1000);                      // wait for a second
+  char key;
+  bool pressed = false;
+
+  for (int j = 0; j < Cols; j++) {
+    digitalWrite(ColPinTbl[j], LOW);
+    for (int i = 0; i < Rows; i++) {
+      if (digitalRead(RowPinTbl[i]) == LOW) {
+        key = keys[i][j];
+      }
+    }
+    digitalWrite(ColPinTbl[j], HIGH);
+  }
+
+  if (key == NULL) {
+    return;
+  }
+
+  Serial.print("pressed: ");
+  Serial.print(key);
 }
